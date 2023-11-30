63b34b22ba403baeb7d67a1a7504ebb600290195
tomo <tomo@crowdeo.com>
2023-11-30 16:33:27 +0700

    mock: SafeERC20 for forceApprove

--- packages/hardhat/contracts/MockEuroe.sol
+++ packages/hardhat/contracts/MockEuroe.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MockEuroe is ERC20, Ownable, ERC20Permit {
+    using SafeERC20 for IERC20;
+    
     constructor(address initialOwner)
         ERC20("MockEuroe", "EUROe")
         Ownable(initialOwner)
@@ -15,6 +18,10 @@ contract MockEuroe is ERC20, Ownable, ERC20Permit {
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+    function freemint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
